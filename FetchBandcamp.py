f75330f0978687dd3951de8bb1c887648bcf6ded
sabjorn <info@sabjorn.net>
2020-12-11 16:08:52 +0100

    add script to automatically search through gmail for bandcamp releases
    features:
    * optional print URLs to file
    * optional mark releases as 'read'

---
+++ FetchBandcamp.py
@@ -0,0 +1,107 @@
+from __future__ import print_function
+import pickle
+import os.path
+import base64
+import argparse
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+from bs4 import BeautifulSoup
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.modify'
+]
+
+def create_service():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('gmail', 'v1', credentials=creds)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Finds unread emails from bandcamp and creates a list of URLs and marks emails as read.")
+    parser.add_argument(
+        "-m",
+        "--mark",
+        help="mark found emails as read",
+        action="store_true")
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="optional: directory for printing files, prints to screen otherwise",
+        type=str,
+        default=None)
+    args = parser.parse_args()
+
+    service = create_service()
+
+    request = service.users().messages().list(userId='me', q="from:noreply@bandcamp.com \"just released\"", labelIds="UNREAD")
+    response = request.execute()
+    messages = response.get('messages', [])
+    while response.get('nextPageToken'):
+        request = service.users().messages().list_next(previous_request=request, previous_response=response)
+        response = request.execute()
+        messages += response['messages']
+
+    if not len(messages):
+        print('No messages found.')
+        exit()
+    
+    urls = set()
+    for message in messages:
+        request = service.users().messages().get(userId='me', id=message['id'], format="raw")
+        response = request.execute()
+        
+        raw = base64.urlsafe_b64decode(response['raw'])
+        raw_cleaned = raw.replace(b"=0D\r\n",b"").replace(b"=\r\n", b"").replace(b"3D",b"")
+
+        soup = BeautifulSoup(raw_cleaned, 'html.parser')
+        for a in soup.find_all('a'):
+            href = a.get('href')
+            if "bandcamp.com/album/" in href:
+                url = href.split('?')[0]
+                urls.add(url)
+
+    try:
+        with open(args.outfile, "w") as f:
+            for url in urls:
+                f.write(url+"\n")
+        print(f"writing to {args.outfile}")
+    except TypeError as e:
+        print("no output file found, printing to screen")
+        for url in urls:
+            print(url)
+        pass
+    except Exception as e:
+        print(e)
+
+    if args.mark:
+        print("marking messages as read")
+        for message in messages:
+            request = service.users().messages().modify(userId="me", id=message['id'], body={"removeLabelIds": ["UNREAD"]})
+            response = request.execute()
---
+++ README.md
@@ -0,0 +1,35 @@
+# FetchBandcampEmails
+## About
+Helper script which uses the Gmail API to find new release emails from Bandcamp.
+
+## Setup
+### Gmail API Credentials
+The easiest way to setup credentials is to go to the Pythong quickstart page:
+
+https://developers.google.com/gmail/api/quickstart/python
+
+Scroll down and click: `Enable the Gmail API`
+
+After accepting everything, download the `credentials.json` file and place it in the top level of this project (i.e. `./FetchBandcampEmails/credentials.json`)
+
+**Note**: **DO NOT SHARE THIS FILE**
+
+### Python
+run:
+```
+python -m venv .venv 
+source .venv/bin/activate
+pip install -r requirements
+```
+
+## Running
+## Basic Command
+```
+python FetchBandcampEmails.py
+```
+
+**Note**: The first time you run the command you will be required to login to your gmail account. A file will be created--`token.pickle`--which is used by the script to prevent the need of loging in again. **DO NOT SHARE THIS FILE**
+
+## Command Line Options
+* `-o`, `--outfile` - *optional* output file for URL list
+* `-m`, `--mark` - *optional* mark found emails as read (remove `UNREAD` label)
