609ebf74941a14a97b256eb343827d4318ea3555
sabjorn <info@sabjorn.net>
2023-03-25 19:58:30 -0700

    add get_metadata script to find tracks from url, download files, and add metadata

--- api/tralbum_details.py
+++ api/tralbum_details.py
@@ -1,15 +1,21 @@
 import requests
+from enum import Enum
+from dataclasses import dataclass, asdict
 
-url = "https://bandcamp.com/api/mobile/25/tralbum_details"
+@dataclass
+class TralbumRequestData:
+    band_id: int
+    tralbum_id: int
+    tralbum_type: str = 't'
 
-payload = {
-  "tralbum_type": "a",
-  "band_id": 535281330,
-  "tralbum_id": 71893326
-}
 
-response = requests.request("POST", url, json=payload)
-track_details = response.json()
+def get_tralbum_details(tralbum_details: TralbumRequestData):
+
+    url = "https://bandcamp.com/api/mobile/25/tralbum_details"
+
+    response = requests.request("POST", url, json=asdict(tralbum_details))
+
+    return response.json()
 
 # track_details.keys() -> dict_keys(['id', 'type', 'title', 'bandcamp_url', 'art_id', 'b
 # and', 'tralbum_artist', 'package_art', 'featured_track_id', 'tracks',
@@ -18,3 +24,9 @@ track_details = response.json()
 # _price', 'price', 'require_email', 'label', 'label_id', 'package_detai
 # ls_lite', 'has_digital_download', 'num_downloadable_tracks', 'merch_so
 # ld_out', 'streaming_limit'])
+if __name__ == "__main__":
+    tralbum_request_data = TralbumRequestData(band_id=310533014, tralbum_id=310533014)
+    tralbum_details = get_tralbum_details(tralbum_request_data)
+
+    print(tralbum_details)
+    
--- get_metadata.py
+++ get_metadata.py
@@ -1,41 +1,179 @@
+from dataclasses import dataclass, asdict
 import sys
+import os
 from time import sleep
+import argparse
+import logging
+from typing import Optional
+from io import BytesIO
+
 import requests
 import json
 from bs4 import BeautifulSoup
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, TRCK, TDRC, TIT2, TALB, TPE1, TPOS, TCON, TCOM, TPOS, TCOP, COMM, TXXX, APIC
 
-def parse_bandcamp_data(text):
+from api.tralbum_details import TralbumRequestData, get_tralbum_details
+
+def parse_bandcamp_data(text: str) -> list[TralbumRequestData]:
     soup = BeautifulSoup(text, 'html.parser')
 
-    band_id = json.loads(soup.select_one('script[data-band]')['data-band'])['id']
+    data_band = soup.select_one('script[data-band]')['data-band']
+    band_id = json.loads(data_band)['id']
 
-    trackinfo = json.loads(soup.select_one('script[data-tralbum]')['data-tralbum'])['trackinfo']
-    track_data = []
-    for track in trackinfo:
-        track_data.append({
-            "band_id": band_id,
-            "tralbum_id": track['track_id'],
-            "tralbum_type": 't',
-        })
+    data_tralbum = soup.select_one('script[data-tralbum]')['data-tralbum']
+    trackinfo = json.loads(data_tralbum)['trackinfo']
+
+    track_data = [TralbumRequestData(band_id=band_id, tralbum_id=track['track_id']) for track in trackinfo]
     return track_data
 
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    
+    parser = argparse.ArgumentParser(description="Takes bandcamp album and track urls and gets metadata")
+    parser.add_argument('-i','--input', nargs='+', help='Provide list of urls to process -- use "-" for stdin', required=True)
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="directory for saving mp3s, default: './'",
+        type=str,
+        default="./")
+    args = parser.parse_args()
+    
+    if args.outfile != "./":
+        os.makedirs(args.outfile, exist_ok=True)
 
-tracks = [];
-for line in sys.stdin:
-    url = line.strip()
-    while True:
-        r = requests.get(url)
+    if args.input and args.input[0] == "-" and len(args.input) == 1:
+        logger.info("reading stdin") 
+        urls = sys.stdin
+    else:
+        urls = args.input
+    
+    # get track data
+    tracks = [];
+    for line in urls:
+        url = line.strip()
+        while True:
+            r = requests.get(url)
 
-        if r.status_code == 404:
+            if r.status_code == 404:
+                break
+            
+            if r.status_code != 200:
+                print(f"status code: {r.status_code}, retrying in 1 second")
+                sleep(1)
+                continue
+
+            tracks += parse_bandcamp_data(r.text)
             break
+
+
+    track_details = [get_tralbum_details(track) for track in tracks]
+    
+
+    @dataclass 
+    class BCPurchase:
+        track_id: int
+        unit_price: float
+        currency: str
+        tralbum_type: str # = 't'
+        track_url: str
+
+    @dataclass
+    class BCMetadata:
+        purchase_data: BCPurchase
+
+        streaming_url: str
+        art_id: int
+
+        # used for mp3 metadata
+        track_title: str
+        album_title: str
+        band_name: str
+        label: Optional[str]
+
+    # create metadata
+    metadata = []
+    for track_detail in track_details:
+        tracks = track_detail.get('tracks')
+        if not tracks:
+            logging.error(f"no tracks in: {track_detail['bandcamp_url']}")
+            continue
         
-        if r.status_code != 200:
-            print(f"status code: {r.status_code}, retrying in 1 second")
-            sleep(1)
+        track = tracks[0] # there should only be one
+        if not track['has_digital_download']:
+            logging.error(f"has no digital download for: {track_detail['bandcamp_url']}")
             continue
 
-        tracks += parse_bandcamp_data(r.text)
-        break
+        if not track['is_purchasable']:
+            logging.error(f"track not purchasable for: {track_detail['bandcamp_url']}")
+            continue
+        
+        purchase_data = {
+            'track_id': track['track_id'],
+            'unit_price': track['price'],
+            'currency': track['currency'],
+            'tralbum_type': track['album_id'],
+            'track_url': track_detail['bandcamp_url'],
+        }
+        data = {
+            'purchase_data': BCPurchase(**purchase_data),
 
-json_out = json.dumps(tracks)
-print(json_out)
+            'streaming_url': track['streaming_url']['mp3-128'],
+            'art_id': track_detail['art_id'],
+
+            'album_title': track_detail['album_title'], 
+            'band_name': track['band_name'],
+            'track_title': track['title'],
+            'label': track['label'],
+        }
+        bc_metadata = BCMetadata(**data)
+        metadata.append(bc_metadata)
+    
+    # download, annotate, and save files
+    for d in metadata:
+        output_filename = f"{d.band_name}-{d.track_title}.mp3".replace(" ", "_").lower()
+        logger.info(f"downloading: {output_filename}")
+
+        # download image
+        r = requests.get(f"https://f4.bcbits.com/img/a{d.art_id}_16.jpg")
+        if r.status_code != 200:
+            logger.error(f"request for {d.streaming_url} ALBUM_ART failed")
+            continue
+        album_art = BytesIO(r.content)
+
+        r = requests.get(d.streaming_url)
+        
+        if r.status_code != 200:
+            logger.error(f"request for {d.streaming_url} failed")
+            continue
+
+        audio_buffer = BytesIO(r.content)
+
+        audio = MP3()
+
+        audio.add_tags()
+
+        audio.tags.add(TIT2(encoding=3, text=d.track_title))
+        audio.tags.add(TALB(encoding=3, text=d.album_title))
+        audio.tags.add(TPE1(encoding=3, text=d.band_name))
+
+        audio.tags.add(APIC(
+                      encoding=3,
+                      mime='image/jpeg',
+                      type=3,
+                      desc=u'Cover',
+                      data=album_art.getvalue()
+                    ))
+
+        pruchase_data = asdict(d.purchase_data)
+        audio.tags.add(COMM(encoding=3, desc='', lang='XXX', text=[json.dumps(purchase_data)]))
+
+        audio.save(audio_buffer)
+        
+        with open(f"/{args.outfile}/{output_filename}", "wb") as f:
+            f.write(audio_buffer.getbuffer())
+
