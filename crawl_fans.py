28303264b76d5214035f553351c81a0afbae2fa1
sabjorn <info@sabjorn.net>
2023-04-08 16:15:50 -0700

    another attempt

--- crawl_fans.py
+++ crawl_fans.py
@@ -27,7 +27,7 @@ def collection_helper(fan_id, output_dir):
         return collection
      
     s = requests.Session()
-    retry = Retry(total=10, connect=5, backoff_factor=2, status_forcelist=Retry.RETRY_AFTER_STATUS_CODES)
+    retry = Retry(total=10, connect=5, backoff_factor=2, status_forcelist=[429])
     adapter = HTTPAdapter(max_retries=retry)
     s.mount('https://', adapter)
     s.headers.update(HEADERS);
